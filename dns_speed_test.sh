7eb2562bb7308e9d29f2c1b5fba763bcbf798a90
iurgnep <63601413+iurgnep@users.noreply.github.com>
2023-02-06 03:56:01 +0800

    Update dns_speed_test.sh

--- dns_speed_test.sh
+++ dns_speed_test.sh
@@ -2,7 +2,6 @@
 
 while IFS= read -r line; do
 [[ $line = \#* ]] && continue
-#printf '%s\n' "$line: $(dig @$line www.google.com | grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2) ms"
 var_sum=0
 for((i=1;i<=10;i++));
 do
@@ -11,8 +10,6 @@ if [ ! $var_t ]; then
 var_t=0
 fi
 let var_sum=$var_sum+$var_t;
-#echo $var_t;
-#echo $var_sum;
 done
 if [ $var_sum -eq 0 ]; then
 echo "$line has no response."
